3095dc91a79401f5c39bf9d7f52e1895925a8dba
J K <alpaca623@gmail.com>
2018-12-28 01:39:19 +0900

    Express Core: Middlewares

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const handleHome = (req, res) => res.send("Hello from my ass!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile!!");
 
+const handleMiddleware = (req, res, next) => {
+    console.log("Between");
+    next();
+}
+
+app.use(handleMiddleware);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
