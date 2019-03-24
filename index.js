00279257f20fd012e4f0282e57856a8e3f21225e
or <alpaca623@gmail.com>
2019-03-24 23:53:01 +0900

    wetube restart..

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-// import express from "express";
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
-
-const handleHome = (req, res) => res.send("Hello from my ass!!");
-
-const handleProfile = (req, res) => res.send("You are on my profile!!");
-
-const handleMiddleware = (req, res, next) => {
-    console.log("Between");
-    next();
+const handleListening = () => {
+  console.log('Listening on : http://localhost:4000');
 }
 
-app.use(handleMiddleware);
-
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+app.listen(4000, handleListening);
