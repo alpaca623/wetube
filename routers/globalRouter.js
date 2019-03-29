01b3fd14e0cd46765a10e7919a76f405b9bd01fa
or <alpaca623@gmail.com>
2019-03-29 12:21:30 +0900

    update join and login view and controller code

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,12 +2,15 @@ import express from 'express';
 
 import routes from '../routes';
 import { search, home } from '../controllers/videoController';
-import { logout, login, join } from '../controllers/userController';
+import { logout, login, getJoin, postJoin } from '../controllers/userController';
 
 const globalRouter = express.Router();
 
+// 같은 url이라 하더라도 get하고 post는 각각 라우팅이 된다.
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout,logout);
 globalRouter.get(routes.search, search);
