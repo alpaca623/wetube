50f62103f2c7e947be270e4f3e63f388c480ce6d
or <alpaca623@gmail.com>
2019-03-29 13:04:59 +0900

    update route.js and connect router and view

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import routes from '../routes';
 import { search, home } from '../controllers/videoController';
-import { logout, login, getJoin, postJoin } from '../controllers/userController';
+import { logout, login, getJoin, postJoin, getLogin, postLogin } from '../controllers/userController';
 
 const globalRouter = express.Router();
 
@@ -10,8 +10,10 @@ const globalRouter = express.Router();
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout,logout);
 globalRouter.get(routes.search, search);
 
