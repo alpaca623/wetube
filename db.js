3beb1397e9d684aa89e2f18a656b16b3932426fc
alpaca623 <alpaca623@gmail.com>
2019-06-03 23:03:40 +0900

    2.22 Home Controller part two

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
     title: "Video Awesome",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 133223,
       name: "Nicolas",
@@ -16,7 +16,7 @@ export const videos = [
     title: "Video Fighting",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 133223,
       name: "Nicolas",
@@ -28,7 +28,7 @@ export const videos = [
     title: "Video Know",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 133223,
       name: "Nicolas",
@@ -40,7 +40,7 @@ export const videos = [
     title: "Video Finish",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 133223,
       name: "Nicolas",
@@ -52,7 +52,7 @@ export const videos = [
     title: "Video Transform",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 133223,
       name: "Nicolas",
