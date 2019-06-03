2d21127d58ba976da74ba99377684fe18890da4e
alpaca623 <alpaca623@gmail.com>
2019-06-03 22:35:38 +0900

    2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+  {
+    id: 1322341,
+    title: "Video Awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 133223,
+      name: "Nicolas",
+      email: "abcd@FCD.d"
+    }
+  },
+  {
+    id: 113332,
+    title: "Video Fighting",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 133223,
+      name: "Nicolas",
+      email: "abcd@FCD.d"
+    }
+  },
+  {
+    id: 5552313,
+    title: "Video Know",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 133223,
+      name: "Nicolas",
+      email: "abcd@FCD.d"
+    }
+  },
+  {
+    id: 12314533,
+    title: "Video Finish",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 133223,
+      name: "Nicolas",
+      email: "abcd@FCD.d"
+    }
+  },
+  {
+    id: 332551,
+    title: "Video Transform",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 133223,
+      name: "Nicolas",
+      email: "abcd@FCD.d"
+    }
+  }
+];
