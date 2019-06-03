493c714b2c635523026d9bc1d5ddfc87f42135e1
alpaca623 <alpaca623@gmail.com>
2019-06-03 17:41:43 +0900

    2.18 Search Controller

--- localMiddleware.js
+++ localMiddleware.js
@@ -1,7 +1,7 @@
 import routes from "./route";
 
 export const localMiddleware = (req, res, next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    next();
-}
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  next();
+};
