727e688d84b88398d66a5e82dc69fb5e89707c60
alpaca <alpaca@alpacaui-MacBook-Pro.local>
2019-06-03 16:34:03 +0900

    add init.js - server start js file

--- init.js
+++ init.js
@@ -1,10 +1,9 @@
-
 import app from "./index";
 
 const PORT = 4000;
 
-const handleListening = () =>{
+const handleListening = () => {
   console.log(`✅ Listening on http://localhost:4000`);
-}
+};
 
 app.listen(PORT, handleListening);
