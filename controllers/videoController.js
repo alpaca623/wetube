dbbe7623aea39fcf334bdca3dee89184b18319b2
alpaca623 <alpaca623@gmail.com>
2019-06-04 00:59:54 +0900

    2.25 More Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -7,11 +7,16 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy, videos});
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) =>{
+  // To do : video upload and redirect video detail
+  res.redirect(routes.videoDetail());
+}
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video detail" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,16 +2,19 @@ import express from "express";
 import routes from "../route";
 import {
   home,
-  upload,
   videoDetail,
   editVideo,
-  deleteVideo
+  deleteVideo,
+  postUpload,
+  getUpload
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.post(routes.upload, postUpload);
+videoRouter.get(routes.upload, getUpload);
+
 videoRouter.get(routes.videos, home);
-videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
