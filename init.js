7dc3a3d155cf8129c0b5ba8ec41276c83380695f
alpaca623 <alpaca623@gmail.com>
2019-06-04 14:18:42 +0900

    3.3 Video Model

--- init.js
+++ init.js
@@ -1,10 +1,14 @@
 import './db';
 import app from "./index";
+import dotenv from 'dotenv';
+dotenv.config();
+// video 스키마 불러오기
+import './models/Video';
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 const handleListening = () => {
-  console.log(`✅ Listening on http://localhost:4000`);
+  console.log(`✅ Listening on http://localhost:${PORT}`);
 };
 
 app.listen(PORT, handleListening);
